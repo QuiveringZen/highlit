b78c79396f893fa188a64dd0e289bda2e4d6e8b2
ELukas <lukas.jukna@mif.stud.vu.lt>
2023-01-04 17:53:55 +0200

    add list support

--- main.ts
+++ main.ts
@@ -1,61 +1,86 @@
-import { Plugin } from 'obsidian'
-import * as CodeMirror from 'codemirror'
+import { Plugin } from 'obsidian';
+import * as CodeMirror from 'codemirror';
 
 export default class HighlitPlugin extends Plugin {
-    private readonly codeMirrorEditors: CodeMirror.Editor[] = [];
-    private previousCursorPosition: CodeMirror.Position = undefined;
+	private previousCursorPosition: CodeMirror.Position = undefined;
+	private readonly codeMirrorEditors: CodeMirror.Editor[] = [];
+	private readonly orderedListRegex: RegExp = new RegExp(
+		'(^[0-9]+.\\s)(\\w*\\s*)*:?\\s?',
+	);
 
-    private readonly registerPreviousCursor = (editor: CodeMirror.Editor) => this.previousCursorPosition = editor.getCursor();
+	private readonly registerPreviousCursor = (editor: CodeMirror.Editor) =>
+		(this.previousCursorPosition = editor.getCursor());
 
-    private readonly handleKeyup = (editor: CodeMirror.Editor, _e: KeyboardEvent) => this.registerPreviousCursor(editor);
-    private readonly handleKeydown = (editor: CodeMirror.Editor, _e: KeyboardEvent) => this.replaceWhileTyping(editor);
+	private readonly handleKeyup = (
+		editor: CodeMirror.Editor,
+		_e: KeyboardEvent,
+	) => this.registerPreviousCursor(editor);
+	private readonly handleKeydown = (
+		editor: CodeMirror.Editor,
+		_e: KeyboardEvent,
+	) => this.replaceWhileTyping(editor);
 
-    async onload() {
-        super.onload();
+	async onload() {
+		super.onload();
+		this.registerCodeMirror((editor) => {
+			this.codeMirrorEditors.push(editor);
+			editor.on('keyup', this.handleKeyup);
+			editor.on('keydown', this.handleKeydown);
+		});
+	}
 
-        this.registerCodeMirror((editor) => {
-            this.codeMirrorEditors.push(editor);
-            editor.on('keyup', this.handleKeyup);
-            editor.on('keydown', this.handleKeydown);
-        });
-    }
+	onunload() {
+		super.onunload();
+		this.codeMirrorEditors.forEach((editor) => {
+			editor.off('keyup', this.handleKeyup);
+			editor.off('keydown', this.handleKeydown);
+		});
+	}
 
-    onunload() {
-        super.onunload();
+	private replaceWhileTyping(editor: CodeMirror.Editor) {
+		if (!this.previousCursorPosition) {
+			return;
+		}
 
-        this.codeMirrorEditors.forEach((editor) => {
-            editor.off('keyup', this.handleKeyup);
-            editor.off('keydown', this.handleKeydown);
-        });
-    }
+		const currentCursorPosition: CodeMirror.Position = editor.getCursor();
 
-    private replaceWhileTyping(editor: CodeMirror.Editor) {
-        if (!this.previousCursorPosition) {
-            return;
-        }
+		const cursorDiff: number =
+			currentCursorPosition.ch - this.previousCursorPosition.ch;
+		const isSameCursor: boolean =
+			currentCursorPosition.line === this.previousCursorPosition.line &&
+			(cursorDiff === 0 || cursorDiff === 1);
 
-        const currentCursorPosition: CodeMirror.Position = editor.getCursor();
+		if (isSameCursor) {
+			const token: CodeMirror.Token = editor.getTokenAt(currentCursorPosition);
 
-        const cursorDiff: number = currentCursorPosition.ch - this.previousCursorPosition.ch;
-        const isSameCursor: boolean =
-            currentCursorPosition.line === this.previousCursorPosition.line
-            && (cursorDiff === 0 || cursorDiff === 1);
+			if (token.string === ':') {
+				const line: number = currentCursorPosition.line;
+				let lineText: string = editor.getLine(line);
+				lineText = lineText.slice(0, lineText.indexOf(':') + 1);
 
-        if (isSameCursor) {
-            const token: CodeMirror.Token = editor.getTokenAt(currentCursorPosition);
+				const isLineAlreadyBolded: boolean = lineText.startsWith('**');
+				const isUnorderedLine: boolean = lineText.startsWith('- ');
+				const isOrderedList: boolean = this.orderedListRegex.test(lineText);
 
-            if (token.string === ':') {
-                const line: number = currentCursorPosition.line;
-                const lineText: string = editor.getLine(line);
-                const isLineAlreadyBolded: boolean = lineText.startsWith('**');
+				const startOfLineOffset: number = 0;
+				let replacementOffset: number = startOfLineOffset;
 
-                if (!isLineAlreadyBolded) {
-                    editor.replaceRange(
-                        `**${lineText}**`,
-                        {line: line, ch: 0},
-                        {line: line, ch: token.end});
-                }
-            }
-        }
-    }
+				if (isUnorderedLine) {
+					replacementOffset = '- '.length;
+					lineText = lineText.slice(replacementOffset);
+				} else if (isOrderedList) {
+					replacementOffset = lineText.indexOf('.', 0) + 2;
+					lineText = lineText.slice(replacementOffset);
+				}
+
+				if (!isLineAlreadyBolded) {
+					editor.replaceRange(
+						`**${lineText}**`,
+						{ line: line, ch: replacementOffset },
+						{ line: line, ch: token.end },
+					);
+				}
+			}
+		}
+	}
 }
